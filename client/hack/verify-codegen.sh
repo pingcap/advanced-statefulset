45d6003f7b55638c68c8eafa598cebbd859a93cd
csuzhangxc <csuzhangxc@gmail.com>
2023-07-11 10:09:46 +0000

    exclude diff output

--- client/hack/verify-codegen.sh
+++ client/hack/verify-codegen.sh
@@ -49,7 +49,7 @@ cp -a "${DIFFROOT}"/* "${TMP_DIFFROOT}"
 "${SCRIPT_ROOT}/hack/update-codegen.sh"
 echo "diffing ${DIFFROOT} against freshly generated codegen"
 ret=0
-diff -Naupr "${DIFFROOT}" "${TMP_DIFFROOT}" || ret=$?
+diff -Naupr --exclude=output "${DIFFROOT}" "${TMP_DIFFROOT}" || ret=$?
 cp -a "${TMP_DIFFROOT}"/* "${DIFFROOT}"
 if [[ $ret -eq 0 ]]
 then
