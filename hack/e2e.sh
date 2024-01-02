442ec915912459452d08051c2a48cdd470f1a79d
Xuecheng Zhang <csuzhangxc@gmail.com>
2024-01-02 15:05:07 +0800

    bump K8s to v1.26.12 (#111)

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -84,7 +84,7 @@ SKIP_UP=${SKIP_UP:-}
 SKIP_DOWN=${SKIP_DOWN:-}
 SKIP_TEST=${SKIP_TEST:-}
 REUSE_CLUSTER=${REUSE_CLUSTER:-}
-KUBE_VERSION=${KUBE_VERSION:-v1.25}
+KUBE_VERSION=${KUBE_VERSION:-v1.26}
 KUBECONFIG=${KUBECONFIG:-~/.kube/config}
 CLUSTER=${CLUSTER:-advanced-statefulset}
 DOCKER_IO_MIRROR=${DOCKER_IO_MIRROR:-}
@@ -101,11 +101,11 @@ echo "DOCKER_IO_MIRROR: $DOCKER_IO_MIRROR"
 echo "KUBE_WORKERS: $KUBE_WORKERS"
 
 declare -A kind_node_images
-kind_node_images["v1.21.14"]="kindest/node:v1.21.14@sha256:8a4e9bb3f415d2bb81629ce33ef9c76ba514c14d707f9797a01e3216376ba093"
 kind_node_images["v1.22.17"]="kindest/node:v1.22.17@sha256:f5b2e5698c6c9d6d0adc419c0deae21a425c07d81bbf3b6a6834042f25d4fba2"
 kind_node_images["v1.23.17"]="kindest/node:v1.23.17@sha256:59c989ff8a517a93127d4a536e7014d28e235fb3529d9fba91b3951d461edfdb"
 kind_node_images["v1.24.15"]="kindest/node:v1.24.15@sha256:7db4f8bea3e14b82d12e044e25e34bd53754b7f2b0e9d56df21774e6f66a70ab" # no image for v1.24.17 yet
 kind_node_images["v1.25.11"]="kindest/node:v1.25.11@sha256:227fa11ce74ea76a0474eeefb84cb75d8dad1b08638371ecf0e86259b35be0c8"
+kind_node_images["v1.26.6"]="kindest/node:v1.26.6@sha256:6e2d8b28a5b601defe327b98bd1c2d1930b49e5d8c512e1895099e4504007adb"
 
 hack::ensure_kind
 hack::ensure_kubectl
